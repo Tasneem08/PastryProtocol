8fe55d0e5575324e807264ee0e1f5db725525c44
Tasneem08 <tasneem08@ufl.edu>
2017-10-20 02:53:15 -0400

    with working methods in

--- project3/lib/MainController.ex
+++ project3/lib/MainController.ex
@@ -1,106 +1,157 @@
-defmodule MainController do
-use GenServer
-# entry point to the code. Read command line arguments and invoke the right things here.
-  # Entry point to the code. 
-  def main(args) do
-    [nNodes,nRequests] = args
-    numNodes=nNodes|>String.to_integer()
-    numRequests=nRequests|>String.to_integer()
+# defmodule MainController do
+# use GenServer
+# # entry point to the code. Read command line arguments and invoke the right things here.
+#   # Entry point to the code. 
+#   def main(args) do
+#     [nNodes,nRequests] = args
+#     numNodes=nNodes|>String.to_integer()
+#     numRequests=nRequests|>String.to_integer()
  
-    # Start up nodes with blank routing table information
-    nodeMap=%{}
-    nodeList=Enum.to_list(1..numNodes)
-    numDigits = Float.ceil(:math.log(numNodes) / :math.log(4)) |> round
-    {nodeMap, nodeList} = loadGenservers(nodeList, numDigits, numNodes, numRequests, nodeMap, [])
-    IO.inspect nodeList
-    # Start gen server
-    start_link(nodeMap, numNodes)
+#     # Start up nodes with blank routing table information
+#     nodeMap=%{}
+#     nodeList=Enum.to_list(1..numNodes)
+#     numDigits = Float.ceil(:math.log(numNodes) / :math.log(4)) |> round
+#     {nodeMap, nodeList} = loadGenservers(nodeList, numDigits, numNodes, numRequests, nodeMap, [])
+#     IO.inspect nodeList
+#     # Start gen server
+#     start_link(nodeMap, numNodes)
 
-    #GenServer.cast(:main_server, {:initiateProtocol, map})
+#     #GenServer.cast(:main_server, {:initiateProtocol, map})
 
-    :timer.sleep(:infinity)
+#     :timer.sleep(:infinity)
 
-  end
+#   end
 
-  def loadGenservers([nodeId|nodeList], numDigits, numNodes, numRequests, nodeMap, newNodeList) do
-   nodeId = Integer.to_string(nodeId,4)|> String.pad_leading(numDigits, "0")
-   newNodeList = List.insert_at(newNodeList, 0, nodeId)
-   {_, pid} = PastryNode.start_link(nodeId, numNodes, numRequests)
-   nodeMap = Map.put(nodeMap,nodeId, pid)
-   loadGenservers(nodeList, numDigits, numNodes, numRequests, nodeMap, newNodeList)
-  end
+#   def loadGenservers([nodeId|nodeList], numDigits, numNodes, numRequests, nodeMap, newNodeList) do
+#    nodeId = Integer.to_string(nodeId,4)|> String.pad_leading(numDigits, "0")
+#    newNodeList = List.insert_at(newNodeList, 0, nodeId)
+#    {_, pid} = PastryNode.start_link(nodeId, numNodes, numRequests)
+#    nodeMap = Map.put(nodeMap,nodeId, pid)
+#    loadGenservers(nodeList, numDigits, numNodes, numRequests, nodeMap, newNodeList)
+#   end
 
-  def loadGenservers([], numDigits, numNodes, numRequests, nodeMap, newNodeList) do
-    {nodeMap, newNodeList}
-  end
+#   def loadGenservers([], numDigits, numNodes, numRequests, nodeMap, newNodeList) do
+#     {nodeMap, newNodeList}
+#   end
   
-  def start_link(nodeMap, numNodes) do
-    GenServer.start_link(MainController, [nodeMap, numNodes], name: :main_server)
-  end
+#   def start_link(nodeMap, numNodes) do
+#     GenServer.start_link(MainController, [nodeMap, numNodes], name: :main_server)
+#   end
 
 
 
 
 
-    def init(map, numNodes, algorithm, count, starttime) do
-      {:ok, {map, numNodes, algorithm, count, starttime}}
-  end
+#     def init(map, numNodes, algorithm, count, starttime) do
+#       {:ok, {map, numNodes, algorithm, count, starttime}}
+#   end
 
-  def handle_cast({:iDied, pid}, state) do
-    [map, numNodes, algorithm, count, starttime] = state
-    if  Map.has_key?(map, pid) do
-    # IO.inspect pid
-      map = Map.delete(map, pid)
+#   def handle_cast({:iDied, pid}, state) do
+#     [map, numNodes, algorithm, count, starttime] = state
+#     if  Map.has_key?(map, pid) do
+#     # IO.inspect pid
+#       map = Map.delete(map, pid)
       
-      count = count + 1
-      spawn(fn->GenServer.cast(:main_server, {:initiateProtocol, map})end)
-    # if count >= Float.floor(0.50*numNodes) do
-    #     diff =  DateTime.diff(DateTime.utc_now, starttime, :millisecond)
-    #     IO.puts "Most nodes have died. Shutting down the protocol.. Convergence took #{diff} milliseconds."
-    #     Process.exit(self(), :shutdown)
-    # end
-    end 
-    {:noreply, [map, numNodes, algorithm,count, starttime]}
-  end
+#       count = count + 1
+#       spawn(fn->GenServer.cast(:main_server, {:initiateProtocol, map})end)
+#     # if count >= Float.floor(0.50*numNodes) do
+#     #     diff =  DateTime.diff(DateTime.utc_now, starttime, :millisecond)
+#     #     IO.puts "Most nodes have died. Shutting down the protocol.. Convergence took #{diff} milliseconds."
+#     #     Process.exit(self(), :shutdown)
+#     # end
+#     end 
+#     {:noreply, [map, numNodes, algorithm,count, starttime]}
+#   end
 
-  def handle_cast({:initiateProtocol, map}, state) do
-      # Process.sleep(50)
-      [_, numNodes, algorithm, count, starttime] = state
-      if map_size(map) <= 0.5*numNodes do
-       diff =  DateTime.diff(DateTime.utc_now, starttime, :millisecond)
-       IO.puts "Most nodes have died. Shutting down the protocol.. Convergence took #{diff} milliseconds."
-       Process.exit(self(), :shutdown)
-      end
-      # IO.inspect "Infecting with #{map_size(map)} alive Nodes..."
-      { pid, firstNode} = Enum.at(map, Enum.random(0..(map_size(map)-1)))
-      #  selectedNeighborNode = String.to_atom("workernode"<>Integer.to_string(firstNode)<>"@"<>GossipNode.findIP())
-      selectedNeighborServer =  String.to_atom("workerserver"<>Integer.to_string(firstNode))
-      # a = DateTime.utc_now
-      if Process.whereis(selectedNeighborServer) != nil do
-      if algorithm == "push-sum" do
-      GenServer.cast(selectedNeighborServer, {:infectPushSum, 0, 0})
-      else
-      GenServer.cast(selectedNeighborServer, {:infect})
-      end
-      end
-      {:noreply, state}
-  end
+#   def handle_cast({:initiateProtocol, map}, state) do
+#       # Process.sleep(50)
+#       [_, numNodes, algorithm, count, starttime] = state
+#       if map_size(map) <= 0.5*numNodes do
+#        diff =  DateTime.diff(DateTime.utc_now, starttime, :millisecond)
+#        IO.puts "Most nodes have died. Shutting down the protocol.. Convergence took #{diff} milliseconds."
+#        Process.exit(self(), :shutdown)
+#       end
+#       # IO.inspect "Infecting with #{map_size(map)} alive Nodes..."
+#       { pid, firstNode} = Enum.at(map, Enum.random(0..(map_size(map)-1)))
+#       #  selectedNeighborNode = String.to_atom("workernode"<>Integer.to_string(firstNode)<>"@"<>GossipNode.findIP())
+#       selectedNeighborServer =  String.to_atom("workerserver"<>Integer.to_string(firstNode))
+#       # a = DateTime.utc_now
+#       if Process.whereis(selectedNeighborServer) != nil do
+#       if algorithm == "push-sum" do
+#       GenServer.cast(selectedNeighborServer, {:infectPushSum, 0, 0})
+#       else
+#       GenServer.cast(selectedNeighborServer, {:infect})
+#       end
+#       end
+#       {:noreply, state}
+#   end
 
-    def handle_cast({:findNextAgain}, state) do
-       GenServer.cast(:main_server, {:initiateProtocol})
-       {:noreply, state}
-  end
+#     def handle_cast({:findNextAgain}, state) do
+#        GenServer.cast(:main_server, {:initiateProtocol})
+#        {:noreply, state}
+#   end
 
-      def handle_cast({:initiateProtocol, map, 0}, state) do
-       [_, _, _, _, starttime] = state
-       diff =  DateTime.diff(DateTime.utc_now, starttime, :millisecond)
-       IO.puts "Most nodes have died. Shutting down the protocol.. Convergence took #{diff} milliseconds."
-       Process.exit(self(), :shutdown)
-  end
+#       def handle_cast({:initiateProtocol, map, 0}, state) do
+#        [_, _, _, _, starttime] = state
+#        diff =  DateTime.diff(DateTime.utc_now, starttime, :millisecond)
+#        IO.puts "Most nodes have died. Shutting down the protocol.. Convergence took #{diff} milliseconds."
+#        Process.exit(self(), :shutdown)
+#   end
 
-  def handle_call({:killMain}, _from, state) do
+#   def handle_call({:killMain}, _from, state) do
     
-    IO.puts "Most nodes have died. Shutting down the protocol..."
-    {:stop, :normal, state}
+#     IO.puts "Most nodes have died. Shutting down the protocol..."
+#     {:stop, :normal, state}
+#   end
+# end
+
+
+defmodule MainController do
+  use GenServer
+
+    # API
+  
+  @doc """
+  """
+  @name :master
+  @base 4
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  @doc """
+  """   
+  def handle_cast(:go, state) do
+    [numNodes, numRequests] = state
+    numBits = round(Float.ceil(:math.log(numNodes)/:math.log(@base)))
+    nodeIDSpace = round(Float.ceil(:math.pow(@base, numBits)))
+    numFirstGroup = if (numNodes <= 1024) do numNodes else 1024 end
+    randList = Enum.shuffle(Enum.to_list(0..(nodeIDSpace-1)))
+    firstGroup = Enum.slice(randList, 0..(numFirstGroup-1))
+
+    list_pid = for nodeID <- firstGroup do
+      {:ok, pid} = PastryNode.start_link(nodeID, numNodes)
+      pid
+    end
+    
+    # First Join
+    for pid <- list_pid do
+      GenServer.cast(pid, {:first_join, firstGroup})
+    end
+    {:noreply, state}
+  end
+
+  def main(args) do
+    [numNodes, numRequests] = args
+    numNodes = String.to_integer(numNodes)
+    numRequests = String.to_integer(numRequests)
+
+    {:ok, master_pid} = start_link([numNodes, numRequests])
+    :global.register_name(@name, master_pid)
+    :global.sync()
+    GenServer.cast(:global.whereis_name(@name), :go)
+    
+    :timer.sleep(:infinity)
   end
 end
--- project3/lib/PastryNode.ex
+++ project3/lib/PastryNode.ex
@@ -1,22 +1,27 @@
 defmodule PastryNode do
 use GenServer
 
-    def start_link(nodeId, numNodes, numRequests) do
-        IO.inspect nodeId
-        GenServer.start_link(PastryNode, [nodeId, numNodes, numRequests], name: :nodeId)
-    end
+    # def start_link(nodeId, numNodes, numRequests) do
+    #     IO.inspect nodeId
+    #     GenServer.start_link(PastryNode, [nodeId, numNodes, numRequests], name: :nodeId)
+    # end
      
-    # Maintains a state  nodeId, numNodes, numRequests, nodeMap, lower, higher, routingTable }
-    def init(nodeId, numNodes, numRequests) do
-        {:ok, {nodeId, numNodes, numRequests, %{}, [], [], [-1,-1,-1,-1] }}
-    end
+    # # Maintains a state  nodeId, numNodes, numRequests, nodeMap, lower, higher, routingTable }
+    # def init(nodeId, numNodes, numRequests) do
+    #     {:ok, {nodeId, numNodes, numRequests, %{}, [], [], [-1,-1,-1,-1] }}
+    # end
 
-    def init(myID, numNodes) do
-      numBits = round(Float.ceil(:math.log(numNodes)/:math.log(@base)))
-      # Initialize routing table to -1
-      rowTab = Tuple.duplicate(-1, @base)
-      routing_table = Tuple.duplicate(rowTab, numBits)
-      {:ok, {myID, numNodes, [], [], routing_table}}
+    use GenServer
+    use Application
+    
+    # API
+  
+    @doc """
+    """
+    @name :master
+    @base 4
+     def start_link(nodeID, numNodes) do
+      GenServer.start_link(__MODULE__, {nodeID, numNodes}, [debug: [:statistics, :trace]])
     end
 
     def samePrefix(nodeID1, nodeID2, bitPos) do
@@ -30,6 +35,78 @@ use GenServer
     def toBaseString(nodeID, len) do
       baseNodeID = Integer.to_string(nodeID, @base)
       String.pad_leading(baseNodeID, len, "0")
+    end
+
+    def addBuffer(myID, firstGroup, numBits, lesserLeaf, largerLeaf, routing_table) do
+
+      if length(firstGroup) == 0 do
+        {lesserLeaf, largerLeaf, routing_table}
+      else
+        nodeID = List.first(firstGroup)
+        
+        # May be added to Larger leaf
+        largerLeaf = if (nodeID > myID && !Enum.member?(largerLeaf, nodeID)) do
+          if(length(largerLeaf) < 4) do
+            largerLeaf ++ [nodeID]
+          else
+            if (nodeID < Enum.max(largerLeaf)) do
+              largerLeaf = List.delete(largerLeaf, Enum.max(largerLeaf))
+              largerLeaf ++ [nodeID]
+            else
+              largerLeaf
+            end
+          end
+        else
+          largerLeaf
+        end
+        
+        # May be added to Lesser leaf
+        lesserLeaf = if (nodeID < myID && !Enum.member?(lesserLeaf, nodeID)) do
+          if(length(lesserLeaf) < 4) do
+            lesserLeaf ++ [nodeID]
+          else
+            if (nodeID > Enum.min(lesserLeaf)) do
+              lesserLeaf = List.delete(lesserLeaf, Enum.min(lesserLeaf))
+              lesserLeaf ++ [nodeID]
+            else
+              lesserLeaf
+            end
+          end
+        else
+          lesserLeaf
+        end
+      
+        # Check routing table
+        samePref = samePrefix(toBaseString(myID, numBits), toBaseString(nodeID, numBits), 0)
+        nextBit = String.to_integer(String.at(toBaseString(nodeID, numBits), samePref))
+        routing_table = if elem(elem(routing_table, samePref), nextBit) == -1 do
+          row = elem(routing_table, samePref)
+          updatedRow = Tuple.insert_at(Tuple.delete_at(row, nextBit), nextBit, nodeID)
+          Tuple.insert_at(Tuple.delete_at(routing_table, samePref), samePref, updatedRow)
+        else
+          routing_table
+        end
+          {lesserLeaf, largerLeaf, routing_table} = addBuffer(myID, List.delete_at(firstGroup, 0), numBits, lesserLeaf, largerLeaf, routing_table)
+      end
+    end
+    
+    # SERVER
+    def init({myID, numNodes}) do
+      numBits = round(Float.ceil(:math.log(numNodes)/:math.log(@base)))
+      # Initialize routing table to -1
+      rowTab = Tuple.duplicate(-1, @base)
+      routing_table = Tuple.duplicate(rowTab, numBits)
+      {:ok, {myID, numNodes, [], [], routing_table}}
+    end
+    
+    @doc """
+    """   
+    def handle_cast({:first_join, firstGroup}, state) do
+      {myID, numNodes, lesserLeaf, largerLeaf, routing_table} = state
+      numBits = round(Float.ceil(:math.log(numNodes)/:math.log(@base)))
+      firstGroup = List.delete(firstGroup, myID)
+      {lesserLeaf, largerLeaf, routing_table} = addBuffer(myID, firstGroup, numBits, lesserLeaf, largerLeaf, routing_table)
+      {:noreply, {myID, numNodes, lesserLeaf, largerLeaf, routing_table}}
     end
 
 end
